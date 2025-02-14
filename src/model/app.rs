9f94b449b1c2124830b825ac59e9f74e11bb5b80
kapiyva <ri9py1201@proton.me>
2025-02-14 11:16:55 +0900

    fix test and bug

--- src/model/app.rs
+++ src/model/app.rs
@@ -17,6 +17,7 @@ pub struct App {
 }
 
 impl App {
+    /// Setup a new App as SelectTable state
     pub fn new() -> Self {
         Self::default()
     }
@@ -61,7 +62,8 @@ impl App {
         }
     }
 
-    /// Change the display state to AddTable
+    /// Call from SelectTable state
+    /// Change the display state to AddTable state
     pub fn set_state_add_table(&mut self) -> Result<()> {
         match &self.display_state {
             DisplayState::SelectTable(SelectTableState { selected_cell }) => {
@@ -70,10 +72,14 @@ impl App {
                 });
                 Ok(())
             }
-            _ => bail!("Cannot change add table state from current state"),
+            _ => bail!(
+                "Cannot change add table state from current state: {:?}",
+                self.display_state
+            ),
         }
     }
 
+    /// Call from AddTable or DisplayTable state
     /// Change the display state to SelectTable
     pub fn set_state_select_table(&mut self) -> Result<()> {
         match &self.display_state {
@@ -94,16 +100,23 @@ impl App {
                 });
                 Ok(())
             }
-            _ => bail!("Cannot change select table state from current state"),
+            _ => bail!(
+                "Cannot change select table state from current state: {:?}",
+                self.display_state
+            ),
         }
     }
 
+    /// Call from DisplayTable state
     /// Change the display state to EditCell
     pub fn set_state_edit_cell(&mut self) -> Result<()> {
         // Only allow changing to EditCell state from DisplayTable state
         let DisplayState::DisplayTable(DisplayTableState { selected_cell }) = &self.display_state
         else {
-            bail!("Cannot change edit cell state from current state");
+            bail!(
+                "Cannot change edit cell state from current state: {:?}",
+                self.display_state
+            );
         };
         self.display_state = DisplayState::EditCell(EditCellState {
             selected_cell: selected_cell.clone(),
@@ -112,10 +125,15 @@ impl App {
         Ok(())
     }
 
+    /// Call from AddTable state
+    /// Add new table and change to DisplayTable state
     pub fn add_table(&mut self, table_name_str: &str) -> Result<()> {
         // Only allow adding table in AddTable state
         let DisplayState::AddTable(_) = self.display_state else {
-            bail!("Cannot add table in current state");
+            bail!(
+                "Cannot add table in current state: {:?}",
+                self.display_state
+            );
         };
 
         let table_name = TableName::from(table_name_str)?;
@@ -130,6 +148,8 @@ impl App {
         Ok(())
     }
 
+    /// Call from SelectTable state
+    /// Select table and change to DisplayTable state
     pub fn select_table(&mut self, table_name_str: &str) -> Result<()> {
         let table_name = TableName::from(table_name_str)?;
         if !self.table_map.contains_key(&table_name) {
@@ -158,19 +178,33 @@ impl App {
                     selected_cell: SelectedCell::new(table_name),
                 });
             }
-            _ => bail!("Cannot select table in current state"),
+            _ => bail!(
+                "Cannot select table in current state: {:?}",
+                self.display_state
+            ),
         };
         Ok(())
     }
 
+    /// Call from DisplayTable display state
+    /// Move the cursor in the table
     pub fn move_cursor(&mut self, row: isize, col: isize) -> Result<()> {
-        // Only allow moving cursor in DisplayTable state
-        let DisplayState::DisplayTable(DisplayTableState { selected_cell }) = &self.display_state
-        else {
-            bail!("Cannot move cursor in current state");
-        };
+        let selected_cell;
+        {
+            // Only allow moving cursor in DisplayTable state
+            let DisplayState::DisplayTable(DisplayTableState { selected_cell: sc }) =
+                &self.display_state
+            else {
+                bail!(
+                    "Cannot move cursor in current state: {:?}",
+                    self.display_state
+                );
+            };
 
-        let table_data = self.get_table_data()?;
+            selected_cell = sc.clone();
+        }
+
+        let table_data = self.get_table_data_mut()?;
         let new_row = (selected_cell.row as isize + row).max(0) as usize;
         let new_col = (selected_cell.col as isize + col).max(0) as usize;
         if new_row < table_data.rows.len() && new_col < table_data.headers.len() {
@@ -181,19 +215,75 @@ impl App {
                     col: new_col,
                 },
             });
-        };
+        }
 
         Ok(())
+    }
+
+    /// Call from DisplayTable state
+    /// Expand the row
+    pub fn expand_row(&mut self) -> Result<()> {
+        let DisplayState::DisplayTable(_) = &self.display_state else {
+            bail!(
+                "Cannot expand row in current state: {:?}",
+                self.display_state
+            );
+        };
+
+        let table_data = self.get_table_data_mut()?;
+
+        table_data.expand_row()
+    }
+
+    /// Call from DisplayTable display state
+    /// Collapse the row
+    pub fn collapse_row(&mut self) -> Result<()> {
+        let DisplayState::DisplayTable(_) = &self.display_state else {
+            bail!(
+                "Cannot collapse row in current state: {:?}",
+                self.display_state
+            );
+        };
+
+        let table_data = self.get_table_data_mut()?;
+        table_data.collapse_row()
+    }
+
+    pub fn expand_col(&mut self) -> Result<()> {
+        let DisplayState::DisplayTable(_) = &self.display_state else {
+            bail!(
+                "Cannot expand col in current state: {:?}",
+                self.display_state
+            );
+        };
+
+        let table_data = self.get_table_data_mut()?;
+        table_data.expand_col()
+    }
+
+    pub fn collapse_col(&mut self) -> Result<()> {
+        let DisplayState::DisplayTable(_) = &self.display_state else {
+            bail!(
+                "Cannot collapse col in current state: {:?}",
+                self.display_state
+            );
+        };
+
+        let table_data = self.get_table_data_mut()?;
+        table_data.collapse_col()
     }
 
     pub fn jump_cursor(&mut self, row: usize, col: usize) -> Result<()> {
         // Only allow jumping cursor in DisplayTable state
         let DisplayState::DisplayTable(DisplayTableState { selected_cell }) = &self.display_state
         else {
-            bail!("Cannot jump cursor in current state");
+            bail!(
+                "Cannot jump cursor in current state: {:?}",
+                self.display_state
+            );
         };
 
-        let table_data = self.get_table_data()?;
+        let table_data = self.get_table_data().ok_or_eyre("table not found")?;
         if row < table_data.rows.len() && col < table_data.headers.len() {
             self.display_state = DisplayState::DisplayTable(DisplayTableState {
                 selected_cell: SelectedCell {
@@ -210,54 +300,112 @@ impl App {
     pub fn get_cell_value(&self) -> Result<String> {
         match &self.display_state {
             DisplayState::DisplayTable(DisplayTableState { selected_cell })
-            | DisplayState::EditCell(EditCellState { selected_cell }) => {
-                let table_data = self.get_table_data()?;
-                Ok(table_data.rows[selected_cell.row][selected_cell.col].clone())
-            }
-            _ => bail!("Cannot get cell value in current state"),
+            | DisplayState::EditCell(EditCellState { selected_cell }) => self
+                .get_table_data()
+                .ok_or_eyre("table not found")?
+                .get_cell(selected_cell.row, selected_cell.col)
+                .map(|s| s.to_string()),
+            _ => bail!(
+                "Cannot get cell value in current state: {:?}",
+                self.display_state
+            ),
         }
     }
 
-    pub fn update_cell_value(&mut self, value: &str) -> Result<()> {
-        let DisplayState::EditCell(EditCellState { selected_cell }) = &self.display_state else {
-            bail!("Cannot update cell value in current state");
-        };
-        let table_data = self
-            .table_map
-            .get_mut(&selected_cell.table_name)
-            .ok_or_eyre(format!(
-                "Table not found: {}",
-                selected_cell.table_name.as_str()
-            ))?;
-        table_data.rows[selected_cell.row][selected_cell.col] = value.to_string();
-        Ok(())
+    pub fn update_cell(&mut self, value: &str) -> Result<()> {
+        let selected_cell;
+        {
+            let DisplayState::EditCell(EditCellState { selected_cell: sc }) = &self.display_state
+            else {
+                bail!(
+                    "Cannot update cell value in current state: {:?}",
+                    self.display_state
+                );
+            };
+
+            selected_cell = sc.clone();
+        }
+
+        self.get_table_data_mut()?
+            .update_cell(selected_cell.row, selected_cell.col, value)
+    }
+
+    fn get_table_data_mut(&mut self) -> Result<&mut TableData> {
+        let table_name = self
+            .get_selected_table_name()
+            .ok_or_eyre("No table selected")?
+            .clone();
+        self.table_map
+            .get_mut(&table_name)
+            .ok_or_eyre(format!("Table not found: {}", table_name.as_str()))
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use eyre::Context;
+
     use super::*;
+
+    fn setup_display_table_app() -> App {
+        let mut app = App::new();
+        // setup table name
+        let table_name = TableName::from("table1").unwrap();
+        // create 2x2 table
+        let mut table_data = TableData::new().unwrap();
+        table_data.expand_row().unwrap();
+        table_data.expand_col().unwrap();
+        table_data.update_cell(0, 0, "value0-0").unwrap();
+        table_data.update_cell(1, 0, "value1-0").unwrap();
+        table_data.update_cell(0, 1, "value0-1").unwrap();
+        table_data.update_cell(1, 1, "value1-1").unwrap();
+        app.table_map.insert(table_name.clone(), table_data);
+        // set display state to DisplayTable
+        app.display_state = DisplayState::DisplayTable(DisplayTableState {
+            selected_cell: SelectedCell::new(table_name),
+        });
+
+        app
+    }
 
     #[test]
     fn test_add_table() {
         let mut app = App::new();
+        // add tables
+        app.set_state_add_table().unwrap();
         app.add_table("table1").unwrap();
-        assert_eq!(
-            app.get_all_table_names(),
-            vec![&TableName::from("table1").unwrap()]
-        );
+        app.set_state_select_table().unwrap();
+        app.set_state_add_table().unwrap();
+        app.add_table("table2").unwrap();
+
+        assert_eq!(app.get_all_table_names().len(), 2);
+        assert!(app
+            .get_all_table_names()
+            .contains(&&TableName::from("table1").unwrap()));
+        assert!(app
+            .get_all_table_names()
+            .contains(&&TableName::from("table2").unwrap()));
     }
 
     #[test]
     fn test_select_table() {
         let mut app = App::new();
+        // add table1
+        app.set_state_add_table().unwrap();
         app.add_table("table1").unwrap();
+        // add table2
+        app.set_state_select_table().unwrap();
+        app.set_state_add_table().unwrap();
         app.add_table("table2").unwrap();
+
+        app.set_state_select_table().unwrap();
         app.select_table("table1").unwrap();
         assert_eq!(
             app.get_selected_table_name(),
             Some(&TableName::from("table1").unwrap())
         );
+
+        app.set_state_select_table().unwrap();
         app.select_table("table2").unwrap();
         assert_eq!(
             app.get_selected_table_name(),
@@ -267,34 +415,40 @@ mod tests {
 
     #[test]
     fn test_move_cursor() {
-        let mut app = App::new();
-        app.add_table("table1").unwrap();
-        app.update_cell_value("value1").unwrap();
-        app.move_cursor(1, 0).unwrap();
-        assert_eq!(app.get_cell_value().unwrap(), "value1");
-        app.move_cursor(0, 1).unwrap();
-        assert_eq!(app.get_cell_value().unwrap(), "value1");
-        app.move_cursor(-1, 0).unwrap();
-        assert_eq!(app.get_cell_value().unwrap(), "value1");
-        app.move_cursor(0, -1).unwrap();
-        assert_eq!(app.get_cell_value().unwrap(), "value1");
+        let mut app = setup_display_table_app();
+
+        // (0,0)
+        assert_eq!(app.get_cell_value().unwrap(), "value0-0");
+        // (1,0)
+        app.move_cursor(1, 0)
+            .with_context(|| {
+                format!(
+                    "move_cursor failed: table:{:?} selected_cell:{:?}",
+                    app.get_table_data(),
+                    app.get_selected_cell()
+                )
+            })
+            .unwrap();
+        assert_eq!(app.get_cell_value().unwrap(), "value1-0");
+        // (1,1)
+        app.move_cursor(0, 1)
+            .with_context(|| format!("move_cursor failed: {:?}", app.get_selected_cell()))
+            .unwrap();
+        assert_eq!(app.get_cell_value().unwrap(), "value1-1");
+        // (0,1)
+        app.move_cursor(-1, 0)
+            .with_context(|| format!("move_cursor failed: {:?}", app.get_selected_cell()))
+            .unwrap();
+        assert_eq!(app.get_cell_value().unwrap(), "value0-1");
     }
 
     #[test]
     fn test_jump_cursor() {
-        let mut app = App::new();
-        app.add_table("table1").unwrap();
-        app.update_cell_value("value1").unwrap();
-        app.jump_cursor(1, 0).unwrap();
-        assert_eq!(app.get_cell_value().unwrap(), "value1");
-        app.jump_cursor(0, 1).unwrap();
-        assert_eq!(app.get_cell_value().unwrap(), "value1");
-    }
+        let mut app = setup_display_table_app();
 
-    #[test]
-    fn test_set_state_add_table() {
-        let mut app = App::new();
-        app.set_state_add_table().unwrap();
-        assert!(matches!(app.get_display_state(), DisplayState::AddTable(_)));
+        app.jump_cursor(1, 0).unwrap();
+        assert_eq!(app.get_cell_value().unwrap(), "value1-0");
+        app.jump_cursor(0, 1).unwrap();
+        assert_eq!(app.get_cell_value().unwrap(), "value0-1");
     }
 }
--- src/model/table.rs
+++ src/model/table.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use eyre::{eyre, Result};
+use eyre::{bail, eyre, Result};
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct TableName(String);
@@ -19,6 +19,7 @@ impl TableName {
     }
 }
 
+#[derive(Debug)]
 pub struct TableData {
     pub headers: Vec<String>,
     pub rows: Vec<Vec<String>>,
@@ -27,12 +28,100 @@ pub struct TableData {
 impl TableData {
     pub fn new() -> Result<Self> {
         Ok(Self {
-            headers: Vec::new(),
-            rows: Vec::new(),
+            headers: vec!["".to_string()],
+            rows: vec![vec!["".to_string()]],
         })
     }
 
     pub fn from_csv(_file_path: PathBuf) -> Result<Self> {
         todo!()
+    }
+
+    pub fn expand_row(&mut self) -> Result<()> {
+        self.rows.push(vec!["".to_string(); self.headers.len()]);
+        Ok(())
+    }
+
+    pub fn collapse_row(&mut self) -> Result<()> {
+        if self.rows.len() > 1 {
+            self.rows.pop();
+        }
+        Ok(())
+    }
+
+    pub fn expand_col(&mut self) -> Result<()> {
+        self.headers.push("".to_string());
+        for row in self.rows.iter_mut() {
+            row.push("".to_string());
+        }
+        Ok(())
+    }
+
+    pub fn collapse_col(&mut self) -> Result<()> {
+        if self.headers.len() > 1 {
+            self.headers.pop();
+            for row in self.rows.iter_mut() {
+                row.pop();
+            }
+        }
+        Ok(())
+    }
+
+    pub fn update_cell(&mut self, row: usize, col: usize, value: &str) -> Result<()> {
+        if row >= self.rows.len() || col >= self.headers.len() {
+            bail!("Invalid row or column index")
+        }
+
+        self.rows[row][col] = value.to_string();
+        Ok(())
+    }
+
+    pub fn get_cell(&self, row: usize, col: usize) -> Result<&str> {
+        if row >= self.rows.len() || col >= self.headers.len() {
+            bail!("Invalid row or column index")
+        }
+
+        Ok(&self.rows[row][col])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_table_name() {
+        let table_name = TableName::from("test_table").unwrap();
+        assert_eq!(table_name.as_str(), "test_table");
+
+        let table_name_err = TableName::from("").unwrap_err();
+        assert_eq!(table_name_err.to_string(), "Table name cannot be empty");
+    }
+
+    #[test]
+    fn test_table_data() {
+        let mut table_data = TableData::new().unwrap();
+        assert_eq!(table_data.headers.len(), 1);
+        assert_eq!(table_data.rows.len(), 1);
+
+        table_data.expand_row().unwrap();
+        assert_eq!(table_data.rows.len(), 2);
+
+        table_data.collapse_row().unwrap();
+        assert_eq!(table_data.rows.len(), 1);
+
+        table_data.expand_col().unwrap();
+        assert_eq!(table_data.headers.len(), 2);
+        assert_eq!(table_data.rows[0].len(), 2);
+
+        table_data.collapse_col().unwrap();
+        assert_eq!(table_data.headers.len(), 1);
+        assert_eq!(table_data.rows[0].len(), 1);
+
+        table_data.update_cell(0, 0, "test").unwrap();
+        assert_eq!(table_data.get_cell(0, 0).unwrap(), "test");
+
+        let cell_err = table_data.get_cell(1, 0).unwrap_err();
+        assert_eq!(cell_err.to_string(), "Invalid row or column index");
     }
 }
