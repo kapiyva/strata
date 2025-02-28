c3b339c6f841e2ebbc814709c170e1468992e887
kapiyva <ri9py1201@proton.me>
2025-02-28 09:37:39 +0900

    impl jump handler

--- src/update/handler/jump_handler.rs
+++ src/update/handler/jump_handler.rs
@@ -1,17 +1,33 @@
 use eyre::{bail, Result};
 
-use crate::{error::StrataError, model::app::App};
+use crate::{
+    error::StrataError,
+    model::app::{state::DisplayFocus, App, AppCommand},
+};
 
 pub(crate) fn jump_handler(app: &mut App) -> Result<()> {
     match app.get_display_focus() {
-        // DisplayFocus::TableView => {
-        //     app.clear_user_input();
-        //     app.focus_command(gen_edit_cell_command());
-        //     Ok(())
-        // }
+        DisplayFocus::TableView => {
+            app.clear_user_input();
+            app.focus_command(gen_command());
+            Ok(())
+        }
         _ => bail!(StrataError::InvalidOperationCall {
             operation: "Jump".to_string(),
-            mode: app.get_display_focus().to_string()
+            focus: app.get_display_focus().to_string()
         }),
     }
+}
+
+fn gen_command() -> AppCommand {
+    AppCommand::new(
+        "Jump",
+        Box::new(|app| {
+            let input = app.get_user_input().to_string();
+            app.jump_cell_selector(&input)?;
+            app.clear_user_input();
+            app.focus_table_view()?;
+            Ok(())
+        }),
+    )
 }
